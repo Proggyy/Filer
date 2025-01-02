c3c58a32b63fe903940ef24da0ce49c998ad68ee
Dev <skrillexmr.skrillex@gmail.com>
2025-01-03 01:19:51 +0400

    Form client validation added.

--- view/Filer.View/src/app/Modules/authentification/login/login.component.ts
+++ view/Filer.View/src/app/Modules/authentification/login/login.component.ts
@@ -15,9 +15,26 @@ export class LoginComponent {
     password: new FormControl("", [Validators.required, Validators.maxLength(50), Validators.minLength(3)])
   });
   public errorText: string = "";
+  onTyping(){
+    this.errorText = "";
+  }
   Login(){
     if(!this.form.valid){
-      this.errorText = "error";
+      let loginErrors = this.form.get("login")?.errors;
+      let passwordErrors = this.form.get("password")?.errors;
+      if(loginErrors?.["required"] || passwordErrors?.["required"]){
+        this.errorText = "Заполните логин и пароль!";
+        return;
+      }
+      if(loginErrors?.["minlength"] || passwordErrors?.["minlength"]){
+        this.errorText = "Логин и пароль должны содержать хотя бы 3 символа!";
+        return;
+      }
+      if(loginErrors?.["maxlength"] || passwordErrors?.["maxlength"]){
+        this.errorText = "Логин или пароль слишком длинные!";
+        return;
+      }
+      this.errorText = "Ошибка";
       return;
     }
     this.auth.Login(this.form.get("login")?.value, this.form.get("password")?.value);
